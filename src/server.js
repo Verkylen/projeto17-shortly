98bb1a50a8e9b4648ca97b3c2be75859a6ba14c9
Renato da Silva Viana <verkylen@gmail.com>
2022-12-20 02:37:17 -0300

    Routes three and four done

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 import { nanoid } from 'nanoid';
 
+// Como o token é recebido no front?
+// Quantos caracteres deve ter o short url?
+// O short url pode/deve ser único?
+
 const server = express();
 server.use(express.json());
 
@@ -103,6 +107,88 @@ server.post('/signin', async (req, res) => {
 });
 
 server.post('/urls/shorten', async (req, res) => {
+    const {headers, body} = req;
+
+    const authorizationSchema = joi.string().pattern(new RegExp('^Bearer [a-f0-9-]+$')).required();
+
+    const authorizationValidation = authorizationSchema.validate(headers.authorization);
+
+    if ('error' in authorizationValidation) {
+        res.sendStatus(401);
+        console.log(authorizationValidation.error.details);
+        return;
+    }
+
+    const token = headers.authorization.replace('Bearer ', '');
+
+    try {
+        const {rows: session} = await connection.query('SELECT * FROM sessions WHERE token = $1;', [token]);
+
+        if (session.length === 0) {
+            res.sendStatus(401);
+            return;
+        }
+
+        const bodySchema = joi.object({
+            url: joi.string().pattern(new RegExp('^https://[^ ]*$')).required()
+        });
+
+        const bodyValidation = bodySchema.validate(body, {abortEarly: false});
+
+        if ('error' in bodyValidation) {
+            res.status(422).send(bodyValidation.error.details);
+            return;
+        }
+
+        const {userId} = session[0];
+
+        const {rows: savedUrl} = await connection.query('SELECT * FROM urls WHERE "userId" = $1 AND url = $2;', [userId, body.url]);
+
+        let shortUrl = '';
+
+        if (savedUrl.length === 0) {
+            shortUrl = nanoid();
+
+            await connection.query(`
+                INSERT INTO urls ("userId", url, "shortUrl") VALUES ($1, $2, $3);
+            `, [userId, body.url, shortUrl]);
+        } else {
+            shortUrl = savedUrl[0].shortUrl;
+        }
+
+        res.status(201).send({shortUrl});
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+server.get('/urls/:id', async (req, res) => {
+    const {id} = req.params;
+
+    const idRegExp = new RegExp('^[1-9][0-9]*$');
+
+    if (idRegExp.test(id) === false) {
+        res.sendStatus(404);
+        return;
+    }
+
+    try {
+        const {rows} = await connection.query('SELECT * FROM urls WHERE id = $1;', [id]);
+
+        if (rows.length === 0) {
+            res.sendStatus(404);
+            return;
+        }
+
+        delete rows[0].userId;
+
+        res.send(rows[0]);
+    } catch {
+        res.sendStatus(500);
+    }
+});
+
+server.get('/urls/open/:shortUrl', async (req, res) => {
 
 });
 
